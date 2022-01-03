b6646b97e80db28a162b1774c54d9579c2c85282
mr-pandabear <94190499+mr-pandabear@users.noreply.github.com>
2022-01-03 02:40:21 -0800

    Update test-node-centos.sh

--- test-node-centos.sh
+++ test-node-centos.sh
@@ -6,6 +6,8 @@ sudo yum -y install automake
 sudo yum -y install libtool
 sudo yum -y install python3-pip
 sudo yum -y install libleveldb-dev
+sudo yum -y install git
+sudo yum -y groupinstall 'Development Tools'
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.6 2
 git clone https://github.com/mr-pandabear/panda-coin.git
 cd panda-coin
@@ -17,3 +19,5 @@ sudo conan install ..
 cd ..
 cmake .
 make server
+sudo sh -c 'curl https://raw.githubusercontent.com/mr-pandabear/panda-utils/master/pandacoin.service > /etc/systemd/system/pandacoin.service'
+sudo service pandacoin start
