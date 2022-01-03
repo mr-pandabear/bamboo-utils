d0d044c8bc1729c503965c3ef0272a57195db065
mr-pandabear <94190499+mr-pandabear@users.noreply.github.com>
2022-01-03 03:26:16 -0800

    Update test-node-centos.sh

--- test-node-centos.sh
+++ test-node-centos.sh
@@ -13,6 +13,8 @@ wget https://download-ib01.fedoraproject.org/pub/epel/8/Everything/x86_64/Packag
 sudo yum localinstall -y leveldb-1.22-1.el8.x86_64.rpm
 wget https://download-ib01.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/l/leveldb-devel-1.22-1.el8.x86_64.rpm
 sudo yum localinstall -y leveldb-devel-1.22-1.el8.x86_64.rpm
+sudo mkdir /usr/lib/x86_64-linux-gnu
+sudo cp /usr/lib64/libleveldb.so.1.22.0 /usr/lib/x86_64-linux-gnu/libleveldb.so
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.6 2
 git clone https://github.com/mr-pandabear/panda-coin.git
 cd panda-coin
